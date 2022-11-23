78ee0aa39d95871f3bc9c0290af3ca80431b829f
hermannmarcell <hermarcist@gmail.com>
2022-11-23 16:53:49 +0100

    work in progress under excercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const { array, number, skipValidation } = require("yargs");
 
+const removeFromArray = function() {
+    arr = arguments[0];
+    finalArr = arr;
+    len = arguments.length;
+    for (i = 1; i < len; i++) {
+        if (typeof arguments[i] == "number" && arguments[i] <= len){
+            index = finalArr.indexOf(arguments[i]);
+            finalArr.splice(index, 1);
+        }
+    }
+    return finalArr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
