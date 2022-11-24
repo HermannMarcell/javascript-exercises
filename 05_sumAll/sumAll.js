26a3fed4aaae351f77bab74b7e3e6f11879ea6f3
hermannmarcell <hermarcist@gmail.com>
2022-11-24 10:21:46 +0100

    solution for excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    finalNum = 0;
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    } else if (typeof a !== "number" || typeof b !== "number") {
+        return "ERROR"
+    } else {
+        if (a < b) {
+            finalNum = a;
+            while (a < b) {
+                ++a;
+                finalNum += a;
+            }
+            return finalNum 
+        } else {
+            finalNum = b;
+            while (b < a) {
+                ++b;
+                finalNum += b;
+            }
+            return finalNum
+        }    
+    } 
 };
 
 // Do not edit below this line
